785e4092de83e5faf16b43abd908c389dcbfd80f
Alek <alek.jelev@abv.bg>
2022-12-26 07:04:56 -0800

    added elements dynamically

--- src/js/app.js
+++ src/js/app.js
@@ -3,18 +3,18 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  document.body.addEventListener("click", () => {
-      const lenght = 5;
-      let count = 5;
-      for(let i = 0; i < length; i++) {
-          let element = document.createElement('article')
+ document.body.addEventListener("click", () => {
+    const lenght = 5;
+    let count = 0;
+    for(let i = 0; i < lenght; i++) {
+        let element = document.createElement('article');
 
-          count+= lenght;
-          element.setAtribute('id', 'article' + count);
-          document.body.appendChild(element);
-          let text = 'sampleText';
-          document.getElementById('article' + count).innerHTML = text;
-          document.getElementById('article' + count).classList.add('message');
-      }
-  });
+        count += lenght;
+        element.setAttribute('id', 'article' + count);
+        document.body.appendChild(element);
+        let text = 'sampleText';
+        document.getElementById('article' + count).innerHTML = text;
+        document.getElementById('article' + count).classList.add('message');
+    }
 });
+  });
